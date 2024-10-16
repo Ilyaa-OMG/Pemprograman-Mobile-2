a538ae926c32ab30b97eac21c3dd9b4f8216172e
Ilyaa-OMG <eyota077@gmail.com>
2024-10-17 05:22:27 +0700

    tugas

--- bin/OOP_Dart/type_check_and_casts.dart
+++ bin/OOP_Dart/type_check_and_casts.dart
@@ -1,30 +1,29 @@
-
 class Employee {
   String name;
   Employee(this.name);
 }
 
-class Manager extends Employee{
+class Manager extends Employee {
   Manager(String name) : super(name);
 }
 
-class VicePresident extends Manager{
+class VicePresident extends Manager {
   VicePresident(String name) : super(name);
 }
 
-void sayHello(Employee employee){
-  if(employee is VicePresident) {
-    VicePresident vicePresident = employee as VicePresident;
+void sayHello(Employee employee) {
+  if (employee is VicePresident) {
+    VicePresident vicePresident = employee;
     print('Hello VP ${vicePresident.name}');
-  }else if(employee is Manager){
-    Manager manager = employee as Manager;
+  } else if (employee is Manager) {
+    Manager manager = employee;
     print('Hello Manager ${manager.name}');
-  }else{
+  } else {
     print('Hello ${employee.name}');
   }
 }
 
-void main(){
+void main() {
   sayHello(Employee('Archovv'));
   sayHello(Manager('Archovv'));
   sayHello(VicePresident('Archovv'));
